6a8c68ab5216420b0dc20467b448bea42ffd79d3
Konstantin Beskrovniy <beskrovnyk@gmail.com>
2019-12-10 15:36:27 +0300

    play audio

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,10 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {interval, Subject} from 'rxjs';
+import {takeUntil, takeWhile} from 'rxjs/operators';
+
+const audio = new Audio('/assets/sound.mp3');
+
+audio.volume = 0;
 
 @Component({
     selector: 'pb-home',
@@ -6,4 +12,49 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
     styleUrls: ['./home.component.less'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {}
+export class HomeComponent {
+    private readonly audio = audio;
+    private readonly hoveredChange$ = new Subject<void>();
+
+    hoveredChange(hovered: boolean) {
+        this.hoveredChange$.next();
+
+        if (hovered) {
+            this.play();
+        } else {
+            this.pause();
+        }
+    }
+
+    private play() {
+        if (this.audio.paused) {
+            this.audio.play();
+        }
+
+        interval(50)
+            .pipe(
+                takeWhile(() => this.audio.volume !== 1),
+                takeUntil(this.hoveredChange$),
+            )
+            .subscribe(() => {
+                this.audio.volume = Math.min(this.audio.volume + 0.01, 1);
+            });
+    }
+
+    private pause() {
+        interval(50)
+            .pipe(
+                takeWhile(() => this.audio.volume !== 0),
+                takeUntil(this.hoveredChange$),
+            )
+            .subscribe(() => {
+                const volume = Math.max(this.audio.volume - 0.01, 0);
+
+                if (volume === 0) {
+                    this.audio.pause();
+                }
+
+                this.audio.volume = volume;
+            });
+    }
+}
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HomeComponent} from './home.component';
+import {HoveredModule} from '../../directives/hovered/hovered.module';
 
 @NgModule({
     declarations: [HomeComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, HoveredModule],
     exports: [HomeComponent],
 })
 export class HomeModule {}
